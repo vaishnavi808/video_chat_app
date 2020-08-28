859941328cf4e61dfbc538307199b2045537bb51
Vaishnavi <vaishnaviborde22@gmail.com>
2020-08-29 00:10:45 +0530

    updated

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,7 @@
 
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals)
